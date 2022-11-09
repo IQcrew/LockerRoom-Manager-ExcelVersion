e5860a5a054cea4391f67fce2547f6333cc9c207
TobiasMikula <94800323+IQcrew@users.noreply.github.com>
2022-11-09 22:04:58 +0100

    optimizingFileSize

--- LockerRoom-Manager/dataManager.cs
+++ LockerRoom-Manager/dataManager.cs
@@ -4,10 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using FireSharp.Config;
-using FireSharp.Interfaces;
-using FireSharp.Response;
-using Newtonsoft.Json;
 
 namespace LockerRoom_Manager
 {
